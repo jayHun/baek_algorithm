1b85b831cbbac1e40f273a3719e92d8f6f7ea3aa
Jhun1992 <raresome92@naver.com>
2017-11-28 20:12:31 +0900

    init_commit

---
+++ src/solution/Sol_11718.java
@@ -0,0 +1,25 @@
+package solution;
+
+import java.util.Scanner;
+
+// Outputs string in a row unit input by user.
+// Maximum input string length is 100.
+// Do not allow blanks each side of input string.
+// Do not allow blank lines.
+
+public class Sol_11718 {
+
+	public static void main(String[] args) {
+	    // 입력한 문자 출력 
+		Scanner sc = new Scanner(System.in);
+	    
+	    while(sc.hasNextLine()){
+	    	String typing = sc.nextLine();
+	        if(typing.isEmpty()||typing.length()>100){
+	        	break;
+	        }else
+	            System.out.println(typing);
+	    }
+	    sc.close();
+	}
+}
---
+++ src/solution/Sol_11719.java
@@ -0,0 +1,30 @@
+package solution;
+
+import java.util.Scanner;
+
+// Outputs string in a row unit input by user.
+// Maximum input string length and line is 100.
+// Allow blanks each side of input string.
+// Allow blank lines.
+
+public class Sol_11719 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str;
+		int cnt=0;
+		Scanner keyboard = new Scanner(System.in);
+		
+		while(cnt<100 && keyboard.hasNext()){
+			str=keyboard.nextLine();
+			if(str.length()>100){
+				cnt--;
+				break;
+			}else{
+				System.out.println(str);
+				cnt++;
+			}
+		}
+		keyboard.close();
+	}
+}
